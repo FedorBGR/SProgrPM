a111a38b3d460219fee88a2940df2f59b031f9ea
FedorBGR <fedor.korolev.2004@mail.ru>
2025-02-10 21:28:50 +0300

    Добавьте файлы проекта.

---
+++ SProgrPM/Database.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Npgsql;
+
+public class Database
+{
+    private string connectionString = "Host=localhost;Username=postgres;Password=Bogorodick200444;Database=PM";
+
+    public DataTable GetPartners()
+    {
+        using (var connection = new NpgsqlConnection(connectionString))
+        {
+            connection.Open();
+            string query = "SELECT company_type, name, director, phone, rating, discount FROM partners";
+            using (var command = new NpgsqlCommand(query, connection))
+            {
+                using (var adapter = new NpgsqlDataAdapter(command))
+                {
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+                    return dt;
+                }
+            }
+        }
+    }
+
+    public void UpdateDiscounts()
+    {
+        using (var connection = new NpgsqlConnection(connectionString))
+        {
+            connection.Open();
+
+            string query = @"
+           UPDATE partners p
+            SET discount = CASE 
+                WHEN COALESCE(s.total_sales, 0) >= 300000 THEN 15
+                WHEN COALESCE(s.total_sales, 0) >= 50000 THEN 10
+                WHEN COALESCE(s.total_sales, 0) >= 10000 THEN 5
+                ELSE 0
+            END
+            FROM (
+                SELECT partner_id, COALESCE(SUM(quantity), 0) AS total_sales
+                FROM sales
+                GROUP BY partner_id
+            ) s
+            WHERE p.id = s.partner_id OR s.partner_id IS NULL;";
+
+            using (var command = new NpgsqlCommand(query, connection))
+            {
+                command.ExecuteNonQuery(); // Выполняем запрос
+            }
+        }
+    }
+
+}
+
---
+++ SProgrPM/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System.Data;
+
+namespace SProgrPM
+{
+    public partial class Form1 : Form
+    {
+        private Database db = new Database();
+        public Form1()
+        {
+            InitializeComponent();
+            LoadPartners();
+        }
+        
+        private void LoadPartners()
+        {
+            db.UpdateDiscounts();
+            DataTable partners = db.GetPartners();
+
+            foreach (DataRow row in partners.Rows)
+            {
+                // Карточка (Panel)
+                Panel card = new Panel
+                {
+                    Width = 840,
+                    Height = 200, // Увеличил высоту для лучшего размещения
+                    BackColor= ColorTranslator.FromHtml("#F4E8D3 "),
+                    BorderStyle = BorderStyle.FixedSingle,
+                    Margin = new Padding(10)
+                };
+
+                
+                Label TypeLabel = new Label
+                {
+                    Text = row["company_type"].ToString(),
+                    Font = new Font("Segoe UI", 16),
+                    ForeColor = Color.Black,
+                    AutoSize = false,
+                    Width = 70,
+                    Height = 25,
+                    Location = new Point(30, 30)
+                };
+
+                Label nameLabel = new Label
+                {
+                    Text = row["name"].ToString(),
+                    Font = new Font("Segoe UI", 16),
+                    ForeColor = Color.Black,
+                    AutoSize = false,
+                    Width = 280,
+                    Height = 30,
+                    Location = new Point(100, 30)
+                };
+
+                
+                Label DirectorLabel = new Label
+                {
+                    Text =  row["director"].ToString(),
+                    Font = new Font("Segoe UI", 14),
+                    ForeColor = Color.Black,
+                    AutoSize = false,
+                    Width = 320,
+                    Height = 25,
+                    Location = new Point(30, 65)
+                };
+
+                Label PhoneLabel = new Label
+                {
+                    Text = row["phone"].ToString(),
+                    Font = new Font("Segoe UI", 14),
+                    ForeColor = Color.Black,
+                    AutoSize = false,
+                    Width = 280,
+                    Height = 20,
+                    Location = new Point(30, 90)
+                };
+
+                Label ratingLabel = new Label
+                {
+                    Text = "Рейтинг: " + row["rating"].ToString(),
+                    Font = new Font("Segoe UI", 14),
+                    ForeColor = Color.Black,
+                    AutoSize = false,
+                    Width = 280,
+                    Height = 20,
+                    Location = new Point(30, 115)
+                };
+
+                // Рейтинг - справа, под адресом
+                Label DiscountLabel = new Label
+                {
+                    Text = row["discount"].ToString() + "%",
+                    Font = new Font("Segoe UI", 14, FontStyle.Bold),
+                    ForeColor = Color.Black,
+                    AutoSize = false,
+                    Width = 280,
+                    Height = 20,
+                    Location = new Point(770, 30)
+                };
+
+                // Добавляем элементы в карточку
+                card.Controls.Add(TypeLabel);
+                card.Controls.Add(nameLabel);
+               card.Controls.Add(DirectorLabel);
+               card.Controls.Add(PhoneLabel);
+                card.Controls.Add(ratingLabel);
+               card.Controls.Add(DiscountLabel);
+
+                // Добавляем карточку в FlowLayoutPanel
+                flowLayoutPanel1.Controls.Add(card);
+            }
+        }
+
+    }
+}
